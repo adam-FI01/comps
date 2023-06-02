dbfb18ecd537eccadff4b080957597a01b79706f
Adam Fine <adamfine@outlook.com>
2023-06-02 16:10:01 -0400

    commit

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class CollectionsHomeComponent {
 
+  data = [
+    {name: 'James', age: 24, job: 'Engineer', employed: true },
+    {name: 'Jill', age: 26, job: 'Engineer', employed: false  },
+    {name: 'Elyse', age: 25, job: 'Designer', employed: true }
+  ];
+
+  header = [
+    {key: 'name', label: 'Name'},
+    {key: 'age', label: 'Age'},
+    {key: 'job', label: 'Job'},
+    {key: 'employed', label: 'Employed'}
+  ]
+
+
 }
