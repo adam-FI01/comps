e3194a61d96770f51b50e9f32df0ce75bd135465
Adam Fine <adamfine@outlook.com>
2023-06-14 21:40:31 -0400

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'collections', loadChildren: () => import('./collections/collections.module').then(s => s.CollectionsModule)},
   {path: 'elements', loadChildren: () => import('./elements/elements.module').then(m => m.ElementsModule) },
+  {path: 'views', loadChildren: () => import('./views/views.module').then(m => m.ViewsModule)},
+  {path: 'mods', loadChildren: () => import('./mods/mods.module').then(m => m.ModsModule)},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: '**', component: NotFoundComponent}
 ];
---
+++ src/app/views/views-home/views-home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-views-home',
+  templateUrl: './views-home.component.html',
+  styleUrls: ['./views-home.component.css']
+})
+export class ViewsHomeComponent {
+
+stats = [
+  { value: 22, label: '# of Users'},
+  { value: 900, label: 'Revenue'},
+  {value: 50, label: 'Reviews'}
+]
+
+items = [
+  {image: '../../../assets/couch.jpeg', title: 'Couch', description: 'This is a couch.'},
+  {image: '../../../assets/dresser.jpeg', title: 'Dresser', description: 'This is a dresser.'},
+]
+
+
+}
