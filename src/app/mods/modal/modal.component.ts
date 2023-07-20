fec0b676a82eb8965dd9c91714bb9941ed55cc3f
Adam Fine <adamfine@outlook.com>
2023-07-19 22:08:46 -0400

    committtt

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+
+ngOnInit() {
+
+  document.body.appendChild(this.elementRef.nativeElement)
+
+}
+
+constructor(private elementRef: ElementRef) {
+
+}
+
 
 }
